3ef63dde895e6819634d60e3246fd9c083c0ae26
Brightvilla <166039620+Brightvilla@users.noreply.github.com>
2025-07-26 14:51:29 +0300

    complete about.js

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const getReadingTimeEmoji = (minutes) => {
+    if (minutes < 30) {
+      const cups = Math.ceil(minutes / 5);
+      return "☕️".repeat(cups);
+    } else {
+      const boxes = Math.ceil(minutes / 10);
+      return "🍱".repeat(boxes);
+    }
+  };
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date} • {getReadingTimeEmoji(minutes)} {minutes} min read</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
